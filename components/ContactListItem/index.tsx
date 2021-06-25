afd2fe03e67dcf555c97dc39455b0cafda3bc30a
hovvahan94@mail.ru <hovvahan94@gmail.com>
2021-06-25 15:56:36 +0400

    create chatRoom

--- components/ContactListItem/index.tsx
+++ components/ContactListItem/index.tsx
@@ -3,19 +3,63 @@ import { View, Text, Image, TouchableWithoutFeedback } from "react-native";
 import { User } from "../../types";
 import styles from "./style";
 import { useNavigation } from "@react-navigation/native";
+import { API, graphqlOperation, Auth } from "aws-amplify";
+import {
+  createChatRoom,
+  createChatRoomUser,
+} from "../../src/graphql/mutations";
 
 export type ContactListItemProps = {
   user: User;
 };
 
 const ContactListItem = (props: ContactListItemProps) => {
-  const {user} = props;
+  const { user } = props;
 
   const navigation = useNavigation();
 
-
-  const onPressHandler = () => {
+  const onPressHandler = async () => {
     // navigate to chat room with this user
+    try {
+      const newChatRoomData = await API.graphql(
+        graphqlOperation(createChatRoom, { input: {} })
+      );
+      console.log(newChatRoomData);
+
+      if (!newChatRoomData.data) {
+        console.log("failed to create chat room");
+        return;
+      }
+
+      const newChatRoom = newChatRoomData.data.createChatRoom;
+
+      await API.graphql(
+        graphqlOperation(createChatRoomUser, {
+          input: {
+            userID: user.id,
+            chatRoomID: newChatRoom.id,
+          },
+        })
+      );
+
+      const userInfo = await Auth.currentAuthenticatedUser();
+
+      await API.graphql(
+        graphqlOperation(createChatRoomUser, {
+          input: {
+            userID: userInfo.attributes.sub,
+            chatRoomID: newChatRoom.id,
+          },
+        })
+      );
+
+      navigation.navigate('ChatRoom', {
+        id: newChatRoom.id,
+        name: "Hardcoded name"
+      })
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -26,9 +70,7 @@ const ContactListItem = (props: ContactListItemProps) => {
 
           <View style={styles.midContainer}>
             <Text style={styles.username}>{user.name}</Text>
-            <Text style={styles.status}>
-              {user.status}
-            </Text>
+            <Text style={styles.status}>{user.status}</Text>
           </View>
         </View>
       </View>
