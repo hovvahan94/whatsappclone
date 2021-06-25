7d220e0406a153579aa28ae96567294dd2aeab48
hovvahan94@mail.ru <hovvahan94@gmail.com>
2021-06-25 12:38:46 +0400

    implement api and user model

--- App.tsx
+++ App.tsx
@@ -1,19 +1,64 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
 
-import Amplify from "aws-amplify";
+import Amplify, { Auth, API, graphqlOperation } from "aws-amplify";
 import { withAuthenticator } from "aws-amplify-react-native";
 import config from "./src/aws-exports";
+import { getUser } from "./src/graphql/queries";
+import { createUser } from "./src/graphql/mutations";
 Amplify.configure(config);
+
+const randomImages = [
+  "https://picsum.photos/200",
+  "https://picsum.photos/200",
+  "https://picsum.photos/200",
+  "https://picsum.photos/200",
+];
 
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const getRandomImage = () => {
+    return randomImages[Math.floor(Math.random() * randomImages.length)];
+  };
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userInfo = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+
+      console.log(userInfo);
+
+      if (userInfo) {
+        const userData = await API.graphql(
+          graphqlOperation(getUser, { id: userInfo.attributes.sub })
+        );
+
+        if (userData.data.getUser) {
+          console.log("User already exists");
+          return;
+        }
+
+        const newUser = {
+          id: userInfo.attributes.sub,
+          name: userInfo.username,
+          imageUri: getRandomImage(),
+          status: "Hey I am useing Whatsapp",
+        };
+
+        await API.graphql(graphqlOperation(createUser, { input: newUser }));
+      }
+    };
+
+    fetchUser();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
